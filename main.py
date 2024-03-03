10fa097016c776c36a2578afdf6bc9d4a91adc2d
Ananthan <ananthan.rajasekharan@gmail.com>
2024-03-04 03:05:47 +0530

    :zap: | Add CORS + Remove Code + ui improvements

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import os
 import requests
 import json
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
 # import uvicorn
 
-
-
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+)
 
 url = "https://eth-mainnet.g.alchemy.com/v2/o7ZRZgZWxUlazl9jLcPvwjAAQbLyx5AF"
 headers = {"Content-Type": "application/json"}
@@ -45,7 +54,7 @@ async def get_block(blockNumber: int):
 
 def filter_transactions(data):
     transactions = data.get('result', {}).get('transactions', [])
-    filtered_transactions = [{'from': tx.get('from'), 'to': tx.get('to')} for tx in transactions]
+    filtered_transactions = [{tx.get('from'): tx.get('to')} for tx in transactions]
     return json.dumps(filtered_transactions)
 # if __name__ == "__main__":
 #     uvicorn.run(app, host="127.0.0.1", port=8000)
